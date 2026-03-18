828ce5fdc9da70d0c8d4432155e702db49fa041d
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-03-18 13:19:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,43 +1,42 @@
 class Solution {
-    
-  private:
-    bool dfsCheck(int node, vector<vector<int>>& adj, vector<int>& vis, vector<int>& pathVis){
-        vis[node] = 1;
-        pathVis[node] = 1;
-        
-        for(auto it : adj[node]){
-            if(!vis[it]){
-                if(dfsCheck(it, adj, vis, pathVis) == true) return true;
-            } else if (pathVis[it] == 1){
-                return true;
-            }
-        }
-        
-        pathVis[node] = 0;
-        return false;
-    }
-    
   public:
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        
+        // code here
         vector<vector<int>> adj(V);
-        vector<int> vis(V, 0);
-        vector<int> pathVis(V, 0);
-        
         for(int i=0; i<edges.size(); i++){
             int u = edges[i][0];
             int v = edges[i][1];
             
             adj[u].push_back(v);
         }
-        
+        vector<int> indegree(V, 0);
         for(int i=0; i<V; i++){
-            if(!vis[i]){
-                if(dfsCheck(i, adj, vis, pathVis) == true){
-                    return true;
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0; i<V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        vector<int> topo;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.push(it);
                 }
             }
         }
+        if(topo.size() != V) return true;
+        
         return false;
     }
 };
