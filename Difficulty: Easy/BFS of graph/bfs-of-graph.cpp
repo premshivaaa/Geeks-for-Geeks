fee6e0328a336c2ac8293fd97584e25f66590a13
Premshiva Chanumuri <143020875+premshivaaa@users.noreply.github.com>
2026-05-11 10:06:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
+++ Difficulty: Easy/BFS of graph/bfs-of-graph.cpp
@@ -1,30 +1,27 @@
 class Solution {
   public:
     vector<int> bfs(vector<vector<int>> &adj) {
-        // code here
-        vector<int> ans;
-        int V = adj.size();
-        vector<int> vis(V, 0);
-        queue<int> q;
+        int n = adj.size();
+        vector<int> vis(n, 0);
         
-        q.push(0);
+        queue<int> q;
+        q.push({0});
         vis[0] = 1;
         
+        vector<int> ans;
+        
         while(!q.empty()){
-            
             int node = q.front();
             q.pop();
             ans.push_back(node);
             
             for(auto it : adj[node]){
                 if(!vis[it]){
-                    q.push(it);
                     vis[it] = 1;
+                    q.push(it);
                 }
             }
-            
         }
-        
         return ans;
     }
 };
